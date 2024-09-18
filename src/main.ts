c2d6dfe702ae86071ebeb560e779f766c0c71698
kase2228 <133229334+kase2228@users.noreply.github.com>
2024-09-19 01:59:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://frontend-of-the-project.onrender.com',  // Replace with your actual frontend URL
+    origin: 'https://frontend-of-the-project.onrender.com',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
